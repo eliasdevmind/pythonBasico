e395bb1c9025436e9c1092cfeac50663db9361c7
eliasdevmin <eliasaraujo1222@outlook.com>
2024-05-06 23:15:40 -0300

    basico python

---
+++ pythonBasico.py
@@ -0,0 +1,22 @@
+#idade, name = (24, 'Elias')
+#print(idade)
+
+#idade, name = (13, 'Andressa')
+#print(idade, name)
+
+""" 
+    Não existe uma variavel para
+    python que declara como const
+    uma variavel mas a um tratado 
+    de boas praticas que variaveis 
+    que sao constante deve ser toda
+    em maiuscula exemplo ('Variavel multavel 'idade)
+    ('Variavel inmultavel 'IDADE), 
+    ter ela toda em maiuscula não
+    garante que ela permanessera const
+    em python mas sim é um identificador 
+    para dizer que o valor não pode ser
+    auterado.
+    
+"""
+#NOME = "Elias Araujo"
