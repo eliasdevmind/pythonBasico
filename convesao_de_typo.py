5edf0c35d513ea36320003a23d280a2b3e742be0
eliasdevmin <eliasaraujo1222@outlook.com>
2024-05-06 23:31:44 -0300

    convercao de valores

---
+++ convesao_de_typo.py
@@ -0,0 +1,18 @@
+print(int(1.5))
+print(int("10"))
+print(float("10.10"))
+print(float(2))
+print(float("100"))
+
+valor = 19
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100/2)
+
+print(100//2)
+
+print(type(100/2))
+
+print(type(100//2))
