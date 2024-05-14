deaed83990bf4aad8eb82ddc2ac2df0bdf78bb94
eliasdevmin <eliasaraujo1222@outlook.com>
2024-05-13 22:21:00 -0300

    oéradores aritimetico aula

---
+++ tipos_de_operadores_pyhon/operadores_aritmeticos.py
@@ -0,0 +1,21 @@
+""" x = 10 - 2 * 5
+print(x)
+
+
+y = (10-2) * 5
+
+print(y)
+
+e = 10 ** 2 * 2
+
+print(e) """
+
+produto_1 = 10 
+produto_2 = 20
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
